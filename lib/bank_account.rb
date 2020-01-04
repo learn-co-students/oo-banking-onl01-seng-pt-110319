73694f511044a9d1b725a2fc7f29226f009753fb
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-04 09:16:09 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
-
+  attr_accessor :balance, :status 
+  attr_reader :name 
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000 
+    @status = "open"
+  end 
+  
+  def deposit(balance)
+    @balance += 1000 
+  end 
+  
+  def display_balance
+    return "Your balance is $#{self.balance}."
+  end 
+  
+  def valid?
+    @balance > 0 && @status == "open" ? true : false 
+  end 
+  
+  def close_account 
+    @status = "closed"
+  end 
 end
